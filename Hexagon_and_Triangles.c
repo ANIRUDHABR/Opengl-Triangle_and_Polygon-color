40685a3358c8c01c802f380f6bcfb2e313660037
B R Anirudha <35489238+ANIRUDHABR@users.noreply.github.com>
2018-02-25 16:12:51 +0530

    Update Hexagon_and_Triangles.c

--- Hexagon_and_Triangles.c
+++ Hexagon_and_Triangles.c
@@ -13,56 +13,56 @@ void display()
 	//Below here the co-ordinates are given in anti-clockwise.
 	glBegin(GL_TRIANGLES);
 		 
-	  glColor3f(0.0,1.0,0.0);		//this gives green color to your triangle
-	  glVertex2f(0.1f, -0.4f);		//vertex points(x, y)
-      glVertex2f(0.25f, -0.7f);
-      glVertex2f(0.4f, -0.4f);
+	  	glColor3f(0.0,1.0,0.0);		   //this gives green color to your triangle
+	  	glVertex2f(0.1f, -0.4f);	    //vertex points(x, y)
+          	glVertex2f(0.25f, -0.7f);
+          	glVertex2f(0.4f, -0.4f);
       
-	  glColor3f(1.54,0.0,1.0);  //similarly pink
-	  glVertex2f(0.4f, -0.4f);
-      glVertex2f(0.8f, -0.4f);
-      glVertex2f(0.6f, 0.0f);      
+	  	glColor3f(1.54,0.0,1.0);          //similarly pink
+	  	glVertex2f(0.4f, -0.4f);
+          	glVertex2f(0.8f, -0.4f);
+          	glVertex2f(0.6f, 0.0f);      
       
-      glColor3f(1.0,0.5,0.0);	//orange
-	  glVertex2f(0.4f, 0.4f);
-      glVertex2f(0.6f, 0.0f);
-      glVertex2f(0.8f, 0.4f);      
+          	glColor3f(1.0,0.5,0.0);	          //orange
+	  	glVertex2f(0.4f, 0.4f);
+          	glVertex2f(0.6f, 0.0f);
+          	glVertex2f(0.8f, 0.4f);      
       
-      glColor3f(0.0,0.0,1.56);	//blue
-	  glVertex2f(0.1f, 0.4f);
-      glVertex2f(0.4f, 0.4f);
-      glVertex2f(0.25f, 0.7f);      
+          	glColor3f(0.0,0.0,1.56);	  //blue
+	  	glVertex2f(0.1f, 0.4f);
+          	glVertex2f(0.4f, 0.4f);
+          	glVertex2f(0.25f, 0.7f);      
       
-      glColor3f(1.5,1.9,0.0);	//yellow
-	  glVertex2f(-0.3f, 0.4f);
-      glVertex2f(-0.1f, 0.0f);
-      glVertex2f(0.1f, 0.4f);      
+          	glColor3f(1.5,1.9,0.0);	          //yellow
+	  	glVertex2f(-0.3f, 0.4f);
+          	glVertex2f(-0.1f, 0.0f);
+          	glVertex2f(0.1f, 0.4f);      
       
-      glColor3f(1.0,0.0,0.0);	//red
-	  glVertex2f(-0.3f, -0.4f);
-      glVertex2f(0.1f, -0.4f);
-      glVertex2f(-0.1f, 0.0f);      
+          	glColor3f(1.0,0.0,0.0);		  //red
+	  	glVertex2f(-0.3f, -0.4f);
+          	glVertex2f(0.1f, -0.4f);
+        	glVertex2f(-0.1f, 0.0f);      
       
-      glEnd();
+        glEnd();
       
 	glBegin(GL_POLYGON);
 		
-		glColor3f(0.0f,1.0f,0.0f);	//This makes vertex green of the hexagon
+		glColor3f(0.0f,1.0f,0.0f);	  //This makes vertex green of the hexagon
 		glVertex2f(0.1f, -0.4f);
 		
-		 glColor3f(1.54,0.0,1.0);	//Similarly this sets vertex pink
+		 glColor3f(1.54,0.0,1.0);	 //Similarly this sets vertex pink
 		glVertex2f(0.4f, -0.4f);
 		
-		glColor3f(1.0,0.5,0.0);		//This sets vertex orange
+		glColor3f(1.0,0.5,0.0);		  //This sets vertex orange
 		glVertex2f(0.6f, 0.0f);
 				
-		glColor3f(0.0,0.0,1.56);	//This sets vertex blue
+		glColor3f(0.0,0.0,1.56);	 //This sets vertex blue
 		glVertex2f(0.4f, 0.4f);
 		
-		glColor3f(1.5,1.9,0.0);		//This sets vertex yellow	
+		glColor3f(1.5,1.9,0.0);		  //This sets vertex yellow	
 		glVertex2f(0.1f, 0.4f);
 		
-		glColor3f(1.0,0.0,0.0);	    //This sets vertex red
+		glColor3f(1.0,0.0,0.0);	          //This sets vertex red
 		glVertex2f(-0.1f, 0.0f);
 	
 	glEnd();	
